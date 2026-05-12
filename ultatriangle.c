fdba3883aa858777762ba7a75d88be2cb5e7fab4
Siddharth <sid13may2005@gmail.com>
2026-05-12 22:59:41 +0530

    Add files via upload

---
+++ ultatriangle.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number of rows :");
+    scanf("%d",&n);
+    /*for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n+1-i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }*/
+   int a=n;
+   for(int i=1;i<=n;i++)
+   {
+    for(int j=1;j<=a;j++)
+    {
+        printf("* ");
+    }
+    a--;
+    printf("\n");
+   }
+    return 0;
+}
