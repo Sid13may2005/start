10ef0a33c59830f2af9f9e51441f0bbb20b68ebf
Siddharth <sid13may2005@gmail.com>
2026-05-22 22:59:54 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("Enter the positive number =");
+    scanf("%d",&x);
+    if(x%2==0)
+    {
+        printf("Even number");
+    }
+    else{
+        printf("odd number");
+    }
+    return 0;
+}
