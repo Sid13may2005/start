ecaa22087eee67c3e099f0269434e1e4de42c0aa
Siddharth <sid13may2005@gmail.com>
2026-06-07 22:30:32 +0530

    Add files via upload

---
+++ half.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the integer = ");
+    scanf("%d",&n);
+    float half=n/2.0;
+    printf("half of integer = %f",half);
+    return 0;
+}
