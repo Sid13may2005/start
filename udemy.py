aa531a1b3eca133be3445596a474365a3e605ce3
Siddharth <sid13may2005@gmail.com>
2026-06-26 23:40:19 +0530

    Add files via upload

---
+++ udemy.py
@@ -0,0 +1,22 @@
+'''
+age=25
+print(type(age))
+age_str=str(age)
+print(type(age_str))
+
+##dyanic typing
+var=10
+print(var,type(var))
+## pyton allows the type of variable to change when program executes
+age=20;
+print(age,type(age))
+age="sid"
+print(age,type(age))
+age=5.11
+print(age,type(age))
+## program ke chalte chalte same variable ka value and type change ho rha
+'''
+
+## by default kya hoga kisi variabel ka value
+tr=input("Input the value")
+print(tr,type(tr))
